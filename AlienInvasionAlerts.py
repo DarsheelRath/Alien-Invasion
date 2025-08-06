eb8d8018eab97580b87a4d53ccda9d782659a343
DarsheelRath <darsheel.rath@gmail.com>
2025-08-06 15:47:15 -0500

    Update

--- AlienInvasionAlerts.py
+++ AlienInvasionAlerts.py
@@ -1,4 +1,5 @@
-class OutOfShipsAlert(Exception):
+Alert = Exception
+class OutOfShipsAlert(Alert):
     """An alert treated like an error to say Out of ships"""
     def __init__(self, message='Game over. Out of ships!'):
         self.message = message
