fdf1663808b7d22dc9203f0a98b8b50b3258b8f2
DarsheelRath <darsheel.rath@gmail.com>
2025-08-07 15:42:54 -0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -54,3 +54,4 @@ class Scoreboard:
         """Draw scores and level to the screen"""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
